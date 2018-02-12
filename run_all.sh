13aed9ffb34e3ee475d74f5acac9c924bf230660
Luc Deniau <luc.deniau@edu.esiee.fr>
2018-02-12 13:52:52 +0100

    Ajout des commandes Docker build sur les 4 containers portant sur le même Dockerfile

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,9 @@
 # !/bin/bash
 
-git clone "https://github.com/mad97231/TP_ansible101"
+git clone "https://github.com/mad97231/tp1_git" share
 
+docker build -f Dockerfile -t compile_code:v1 compile_code/
+docker build -f Dockerfile -t execute_code:v1 execute_code/
+docker build -f Dockerfile -t git_stat:v1 git_stat/
+docker build -f Dockerfile -t publish_stat:v1 publish_stat/
 
